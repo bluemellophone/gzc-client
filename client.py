bf9db48079eab7c9dde42cfe32f46ed00d786dc1
Hendrik Weideman <weideh@rpi.edu>
2015-01-29 16:48:47 -0500

    Added a file containing dummy gps data and code that POSTs this dummy data to the gzc-server for testing.

--- client.py
+++ client.py
@@ -1,13 +1,15 @@
 from __future__ import absolute_import, division, print_function
 import requests
 import simplejson as json
+from os.path import join, exists  # NOQA
 
 # This is test code for pushing POST content and a zip file to a web server.
 # Run server.py from https://github.com/bluemellophone/gzc-server and then run this
 # client.
 
 DOMAIN = 'http://localhost:5000'
-URL = DOMAIN + '/images/submit'
+IMGURL = DOMAIN + '/images/submit'
+GPSURL = DOMAIN + '/gps/submit'
 
 # LOAD THE POST VARIABLES
 data = {
@@ -18,6 +20,7 @@ data = {
     'image_first_time_minute': 36,
 }
 
+# image data
 # LOAD THE ZIP AND ADD TO THE PAYLOAD
 content = open('test.zip', 'rb')
 files = {
@@ -25,9 +28,21 @@ files = {
 }
 
 # SEND POST REQUEST WITH data AND files PAYLOADS
-r = requests.post(URL, data=data, files=files)
+r = requests.post(IMGURL, data=data, files=files)
 
 # Response
 print("HTTP STATUS:", r.status_code)
 response = json.loads(r.text)
 print("RESPONSE:", response)
+
+# gps data
+content = open(join('dummy_gps', 'dummy_gps.csv'), 'rb')
+files = {
+    'gps_data': content,
+}
+
+r = requests.post(GPSURL, data=data, files=files)
+print("HTTP STATUS:", r.status_code)
+response = json.loads(r.text)
+print("RESPONSE:", response)
+
