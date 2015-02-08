c53f1743196b80505137e4d36ee3b05f4f6ce389
danlowe95 <danlowe95@gmail.com>
2015-02-08 14:01:47 -0500

    main_client.py combined image-upload with gps-upload in a tabbed window.  Feel free to change anything in it, it's just what I have made so far and it is very basic.

---
+++ main_client.py
@@ -0,0 +1,39 @@
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+import sys
+import image_import_client as iic
+import gpsclient as gc
+def main():
+    
+    app = QtGui.QApplication(sys.argv)
+    tabs= QtGui.QTabWidget()
+    
+    # image_import tab
+    MainWindow = QtGui.QMainWindow()
+    ui = iic.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    tabs.addTab(MainWindow, "Images")
+
+    # GPS_import tab
+
+    # QAPP = QtGui.QApplication(sys.argv)
+    wgt = gc.GPSGuiWidget()
+    wgt.show()
+    # QAPP.setActiveWindow(wgt)
+    # QAPP.exec_()
+    tabs.addTab(wgt,"GPS")
+
+    #Resize width and height
+    tabs.resize(800, 600)
+    
+    #Move QTabWidget to x:300,y:300
+    tabs.move(300, 300)
+    
+    tabs.setWindowTitle('\'TheGreat Zebra Count Image Upload\'')
+    tabs.show()
+    
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
