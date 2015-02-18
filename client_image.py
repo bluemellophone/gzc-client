85aeb7976a2a75feabdba52f3aef0615141c9c0a
Jason Parham <bluemellophone@gmail.com>
2015-02-18 11:20:40 -0500

    Updated colors

--- client_image.py
+++ client_image.py
@@ -140,21 +140,29 @@ class user_input(QtGui.QWidget):
 
     def init_widgets(self):
         self.logo = QtGui.QLabel(self)
-        self.logo.setPixmap(QtGui.QPixmap("assets/logo.png").scaled(QtCore.QSize(100, 100)))
+        self.logo.setPixmap(QtGui.QPixmap('assets/logo.png').scaled(QtCore.QSize(100, 100)))
 
-        self.browse_label = QtGui.QLabel("1) Select Drive With SD card:", self)
-        self.browse_button = QtGui.QPushButton("Browse", self)
+        self.browse_label = QtGui.QLabel('1) Select Drive With SD card:', self)
+        self.browse_button = QtGui.QPushButton('Browse', self)
         self.browse_text = QtGui.QLineEdit(self)
 
-        self.id_label = QtGui.QLabel("2) Input Identification Information", self)
-        self.id_car_label = QtGui.QLabel("Car Number:", self)
-        self.id_person_label = QtGui.QLabel("ID Letter:")
+        self.id_label = QtGui.QLabel('2) Input Identification Information', self)
+        self.id_car_label = QtGui.QLabel('Car Number:', self)
+        self.id_person_label = QtGui.QLabel('ID Letter:')
 
-        self.colorList = ["Red", "Green", "Blue", "Yellow", "Black", "White"]
-        bgList = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 0, 0), (255, 255, 255)]
+        self.colorList = [
+            ('white',  '#FFFFFF'),
+            ('red',    '#D9534F'),
+            ('orange', '#EF7A4C'),
+            ('yellow', '#F0AD4E'),
+            ('green',  '#5CB85C'),
+            ('blue',   '#3071A9'),
+            ('purple', '#6F5499'),
+            ('black',  '#333333'),
+        ]
         self.colorBox = QwwColorComboBox()
-        for color_name, rgb in zip(self.colorList, bgList):
-            color = QtGui.QColor(rgb[0], rgb[1], rgb[2])
+        for (color_name, color_hex) in self.colorList:
+            color = QtGui.QColor(color_hex)
             self.colorBox.addColor(color, color_name)
         #self.id_car_color = QButtonGroup(self)
         #self.id_car_color_buttons = []
@@ -169,17 +177,17 @@ class user_input(QtGui.QWidget):
         self.id_person = QtGui.QComboBox(self)
         self.id_person.addItems(list(string.ascii_uppercase))
 
-        self.sync_label = QtGui.QLabel("3) Synchronize Image Infromation", self)
-        self.sync_number_label = QtGui.QLabel("First Image Number:", self)
-        self.sync_time_label = QtGui.QLabel("First Image Timestamp:", self)
+        self.sync_label = QtGui.QLabel('3) Synchronize Image Infromation', self)
+        self.sync_number_label = QtGui.QLabel('First Image Number:', self)
+        self.sync_time_label = QtGui.QLabel('First Image Timestamp:', self)
 
         self.sync_number = QtGui.QSpinBox(self)
         self.sync_number.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
         self.sync_time = QtGui.QTimeEdit(self)
         self.sync_time.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
 
-        self.import_label = QtGui.QLabel("4) Import", self)
-        self.import_button = QtGui.QPushButton("Import", self)
+        self.import_label = QtGui.QLabel('4) Import', self)
+        self.import_button = QtGui.QPushButton('Import', self)
 
     def init_layout(self):
         browse_upper = QtGui.QHBoxLayout()
@@ -261,15 +269,15 @@ class user_input(QtGui.QWidget):
         self.import_button.clicked.connect(self.import_)
 
     def open_directory(self):
-        directory = str(QtGui.QFileDialog.getExistingDirectory(self, "Select Directory"))
+        directory = str(QtGui.QFileDialog.getExistingDirectory(self, 'Select Directory'))
         self.browse_text.setText(directory)
 
     def import_(self):
         directory = self.browse_text.text()
         files = listdir(directory)
-        target_directory = path.join("user_photos", str(self.colorBox.currentText()) + str(self.id_car_number.value()), str(self.id_person.currentText()))
+        target_directory = path.join('user_photos', str(self.colorBox.currentText()) + str(self.id_car_number.value()), str(self.id_person.currentText()))
         self.copyThread = CopyThread(directory, files, [target_directory])
-        self.connect(self.copyThread, QtCore.SIGNAL("file_done"), self.parent().update_recent_file)
+        self.connect(self.copyThread, QtCore.SIGNAL('file_done'), self.parent().update_recent_file)
         self.copyThread.start()
 
 
@@ -289,7 +297,7 @@ class image_import_interface(QtGui.QWidget):
         self.scroll_area.setWidget(self.image_selection_group)
         self.scroll_area.setHorizontalScrollBarPolicy(1)
         self.progress_bar = QtGui.QLineEdit(self)
-        self.submit_button = QtGui.QPushButton("Submit and Upload", self)
+        self.submit_button = QtGui.QPushButton('Submit and Upload', self)
 
     def init_layout(self):
         self.uber_layout = QtGui.QVBoxLayout()
@@ -309,7 +317,7 @@ class image_import_interface(QtGui.QWidget):
         DOMAIN = 'http://localhost:5000/images/submit'
         #Zip selected images: first, last, zebra/, giraffe/
         chdir(path.dirname(path.realpath(__file__)))
-        pull_directory = path.join("user_photos", str(self.user_input_group.colorBox.currentText()) + str(self.user_input_group.id_car_number.value()), str(self.user_input_group.id_person.currentText()))
+        pull_directory = path.join('user_photos', str(self.user_input_group.colorBox.currentText()) + str(self.user_input_group.id_car_number.value()), str(self.user_input_group.id_person.currentText()))
         chdir(path.join(getcwd(), pull_directory))
 
         first = self.image_selection_group.first_image.get_selection()[0]
@@ -317,7 +325,7 @@ class image_import_interface(QtGui.QWidget):
         giraffe = []
         for IB in self.image_selection_group.image_boxes:
             selection = IB.get_selection()
-            if selection[1] == "Zebra":
+            if selection[1] == 'Zebra':
                 zebra.append(selection[0])
             else:
                 giraffe.append(selection[0])
@@ -352,12 +360,12 @@ class image_import_interface(QtGui.QWidget):
         r = requests.post(DOMAIN, data=data, files=files)
 
         # Response
-        print("HTTP STATUS:", r.status_code)
+        print('HTTP STATUS:', r.status_code)
         response = json.loads(r.text)
-        print("RESPONSE:", response)
+        print('RESPONSE:', response)
 
     def update_recent_file(self, filename):
-        self.progress_bar.setText("Imported new image to " + filename)
+        self.progress_bar.setText('Imported new image to ' + filename)
         self.image_selection_group.add_filename(filename)
 
 
