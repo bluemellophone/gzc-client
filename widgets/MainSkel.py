bf6fb9abc194b5b2140e95590002779c5e3741bc
Jason Parham <bluemellophone@gmail.com>
2015-02-23 12:56:00 -0500

    Sidebar minimum width

--- widgets/MainSkel.py
+++ widgets/MainSkel.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'Main.ui'
 #
-# Created: Mon Feb 23 12:24:27 2015
+# Created: Mon Feb 23 12:55:28 2015
 #      by: PyQt4 UI code generator 4.11.3
 #
 # WARNING! All changes made in this file will be lost!
